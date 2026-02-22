0e95f0fdbfcb5eeb01c4b63bc211dc9369450de9
Fernando Regodeceves <fregodecevesleiva@gmail.com>
2026-02-22 11:42:50 -0300

    feat: api data to table

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,10 +1,11 @@
 import requests
 import json
+import pandas as pd
 
 url = 'https://archive-api.open-meteo.com/v1/archive'
 parameters = {
-    "latitude": 40.4168,
-    "longitude": -3.7038,
+    "latitude": -36.7,
+    "longitude": -71.9,
     "start_date": "2024-01-01",
     "end_date": "2024-01-05",
     "daily": ["temperature_2m_max", "precipitation_sum"],
@@ -17,8 +18,15 @@ print(f"Status Code: {answer.status_code}")
 
 if answer.status_code == 200:
     raw_data = answer.json()
-    print("Data structure:")
-    print(json.dumps(raw_data, indent=2))    
+    daily_data = raw_data['daily']
+    df = pd.DataFrame(raw_data['daily'])
+    print('Data to Table')
+    print(df.head())  
+    print('-'*50)
+
+    print('datatypes')
+    print(df.dtypes)
+    print('-'*50)
 else:
     print(f"Error: {answer.status_code}")
 
